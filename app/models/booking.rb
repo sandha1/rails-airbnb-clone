5ad3d22af8ca0706439594c6844fec3ec555b34c
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-26 15:15:56 +0100

    Added some validations to my models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :flat
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  after_validation :calculate_total_price
+
+  def calculate_total_price
+    total_days = end_date - start_date
+    self.total_price = total_days * flat.price_per_night
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,6 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericability: { only_integer: true }
 end
