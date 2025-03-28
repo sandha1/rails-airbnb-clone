4fdaecc1f002abb43c0d1f917b10dae4d39844da
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-03-28 17:04:22 +0100

    Added Cloudinaray storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ user1 = User.create!(email: "jane@gmail.com", password: "fakersand")
 user2 = User.create!(email: "fred@gmail.com", password: "fakersand")
 user3 = User.create!(email: "sarah@gmail.com", password: "fakersand")
 
-puts "Created my 3 users."
+puts "Created #{User.count} users."
 
 12.times do
   Flat.create!(name: Faker::Book.unique.title, description: Faker::Lorem.paragraph, address: Faker::Address.full_address, price_per_night: Faker::Number.within(range: 50..200), user: user1)
@@ -19,4 +19,4 @@ end
   Flat.create!(name: Faker::Book.unique.title, description: Faker::Lorem.paragraph, address: Faker::Address.full_address, price_per_night: Faker::Number.within(range: 50..200), user: user2)
 end
 
-puts "Created my 15 flats."
+puts "Created #{Flat.count} flats."
